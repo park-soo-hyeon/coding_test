d4958ec922f4e392373e82ed867b711809bcea4e
ParkSoohyeon <130148659+park-soo-hyeon@users.noreply.github.com>
2026-04-20 22:40:02 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
@@ -0,0 +1,13 @@
+def solution(A, B):
+    if(A == B):
+        return 0
+    
+    result = A[len(A)-1]+A[0:len(A)-1]
+        
+    for i in range(len(A)+1):
+        if (result == B):
+            return i+1
+        else:
+            answer = -1
+            result = result[len(A)-1]+result[0:len(A)-1] 
+    return answer
