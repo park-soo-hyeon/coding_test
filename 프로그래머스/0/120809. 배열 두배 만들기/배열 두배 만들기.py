ae3bc75b44a154fc61edab86838f9c12caf325f2
ParkSoohyeon <130148659+park-soo-hyeon@users.noreply.github.com>
2026-04-21 20:36:32 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.06 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
@@ -1,8 +1,2 @@
 def solution(numbers):
-    answer = []
-    
-    for i in range(0, len(numbers), 1):
-        answer.append(1)
-        answer[i] = numbers[i]*2
-    
-    return answer
+    return [num*2 for num in numbers]
