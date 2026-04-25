e01c2ab5f5bfe3a3dd875225af09a0894dcb61da
ParkSoohyeon <130148659+park-soo-hyeon@users.noreply.github.com>
2026-04-25 18:38:42 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 7.23 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
@@ -1,13 +1,2 @@
 def solution(myString):
-    answer = []
-    check = 0
-    
-    for i in range(0, len(myString), 1):
-        if myString[i] == 'x':
-            answer.append(myString[check:i])
-            check = i+1
-    if myString[check:]:
-        answer.append(myString[check:])
-    answer = list(filter(None, answer))
-    answer.sort()
-    return answer
+    return sorted(i for i in myString.split('x') if i)
