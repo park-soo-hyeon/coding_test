9c84ec62566c7fdb7de09b8a5f5bab78b8ce507e
ParkSoohyeon <130148659+park-soo-hyeon@users.noreply.github.com>
2026-05-01 09:25:03 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.03 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -1,4 +1,2 @@
 def solution(my_string):
-    answer = [int(char) for char in my_string if char.isdigit()]
-    answer.sort()
-    return answer
+    return sorted([int(char) for char in my_string if char.isdigit()])
