94e7a4fab5df8eadd7c7e0e1c15551456456b9e9
ParkSoohyeon <130148659+park-soo-hyeon@users.noreply.github.com>
2026-05-03 22:37:47 +0900

    [level 0] Title: 암호 해독, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.py
@@ -1,6 +1,2 @@
 def solution(cipher, code):
-    answer = ''
-    digit_list = list(map(str, str(cipher)))
-    for i in range(code-1, len(digit_list), code):
-        answer += digit_list[i]
-    return answer
+    return cipher[code-1::code]
