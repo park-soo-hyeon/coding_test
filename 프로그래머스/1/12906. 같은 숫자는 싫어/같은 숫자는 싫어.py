43bf9a175af89ff9b2fb27dc62e1007b988bb4da
ParkSoohyeon <130148659+park-soo-hyeon@users.noreply.github.com>
2026-05-06 00:43:34 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 117.28 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,4 +1,9 @@
 from itertools import groupby
 
 def solution(arr):
-    return [key for key, group in groupby(arr)]
+    answer = []
+    for i in arr:
+        if len(answer) == 0 or answer[-1] != i:
+            answer.append(i)
+
+    return answer
