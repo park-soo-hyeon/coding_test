92903d949a3cd5e3ce23017f5403092ee712ffdb
ParkSoohyeon <130148659+park-soo-hyeon@users.noreply.github.com>
2026-05-12 20:04:44 +0900

    [level 0] Title: 옹알이 (1), Time: 0.08 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -0,0 +1,12 @@
+def solution(babbling):
+    babb = ["aya", "ye", "woo", "ma"]
+    answer = 0
+    
+    for i in babbling:
+        result = 0
+        for j in range(4):
+            if i.find(babb[j]) != -1:
+                result += len(babb[j])
+        if len(i) == result:
+            answer += 1
+    return answer
